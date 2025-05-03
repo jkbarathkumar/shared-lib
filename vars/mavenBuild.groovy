994e5c32ef48e533651805246dc46f215ac013c6
Barathkumar <140275835+jkbarathkumar@users.noreply.github.com>
2025-05-03 15:10:07 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,15 +1,6 @@
 def call(Map config = [:]) {
     def mvnCmd = config.get('mavenCmd', 'mvn clean install')
 
-    pipeline {
-        agent any
-        stages {
-            stage('Build') {
-                steps {
-                    echo "Running Maven Command: ${mvnCmd}"
-                    sh mvnCmd
-                }
-            }
-        }
-    }
+    echo "Running Maven Command: ${mvnCmd}"
+    sh mvnCmd
 }
